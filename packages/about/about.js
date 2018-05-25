90673bc01b2a6bbceecb93d86d75c0395b9de913
Olga Shamirzaeva <utka-aka-ireth@yandex.ru>
2018-05-25 18:13:54 +0700

    add decription

--- packages/about/about.js
+++ packages/about/about.js
@@ -1,13 +1,17 @@
-const { div, h2, p } = require('react-dom-factories');
+const { div, h2, p, ul, li } = require('react-dom-factories');
 const description = require('core/description.js');
 
 
 const About = () => div({ className: 'about' }, [
-  h2({ className: 'about__header' }, 'Something about "to do list"'),
+  h2({ className: 'about__header' }, 'Pink Panther\'s "to do list"'),
   p({ className: 'about__text' }, description.descriptionText.intro),
-  div({ className: 'task__color' }),
-  div({ className: 'task__color task__color_high-priority' }),
-  p({ className: 'about__text' }, description.descriptionText.priority),
+  ul({ className: 'about__instruction', key: 'task-instruction' }, description.descriptionText.instr, [
+    li({ className: 'about__text' }, description.descriptionText.add),
+    li({ className: 'about__text' }, description.descriptionText.priority),
+    li({ className: 'about__text' }, description.descriptionText.edit),
+    li({ className: 'about__text' }, description.descriptionText.done),
+    li({ className: 'about__text' }, description.descriptionText.delete),
+  ]),
 ]);
 
 
--- packages/core/locale.js
+++ packages/core/locale.js
@@ -3,5 +3,6 @@ module.exports = {
     DEFAULT_ERROR: 'Something\'s gone wrong.',
     CAN_NOT_FIND_TASK: 'The task is not found.',
   },
-  empty: 'You don\'t have any tasks yet.',
+  empty: 'You don\'t have any new tasks yet. Enjoy your day.',
+  emptyDone: 'There\'s nothing here yet but I believe in you!',
 };
